1e4b3c018c442b94928d1b3d479faf3459b0d262
wangjiawei601996 <wangjiawei601996@pwrd.com>
2019-10-17 15:27:03 +0800

    feat: 上传项目代码

---
+++ Landmarks/MapVIew.swift
@@ -0,0 +1,21 @@
+//
+//  MapVIew.swift
+//  Landmarks
+//
+//  Created by wangjiawei on 2019/10/17.
+//  Copyright © 2019 wangjiawei. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        MapVIew()
+    }
+}
