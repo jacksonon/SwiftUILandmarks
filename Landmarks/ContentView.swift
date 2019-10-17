e1f63e9b58d6caf9054f527816dabc8487501625
wangjiawei601996 <wangjiawei601996@pwrd.com>
2019-10-17 14:41:59 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by wangjiawei on 2019/10/17.
+//  Copyright © 2019 wangjiawei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
